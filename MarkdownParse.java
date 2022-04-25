eab5d92f9d4f0664092daa869f6fac4332b5c14e
Lucia-Y <lucia930309@gmail.com>
2022-04-24 20:03:51 -0700

    Update

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -19,6 +19,9 @@ public class MarkdownParse {
             if (openParen == -1) {
                 break;
             }
+            if (openBracket == -1) {
+                break;
+            }
             toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
         }
